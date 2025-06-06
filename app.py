dad55be5ed6f3063d41d6732f921f03564dd8fbe
Buggimonster <buggipost@hotmail.com>
2025-06-06 20:02:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,27 +37,9 @@ st.markdown("""
         color: black !important;
     }
 
-    /* ALLERSIDSTE FORSØG: Styler "Download"-knappen for alle tilstande */
-    div[data-testid="stDownloadButton"] > a,
-    div[data-testid="stDownloadButton"] > a:link,
-    div[data-testid="stDownloadButton"] > a:visited {
-        border: 2px solid #C00 !important;
-        border-radius: 5px !important;
+    /* SIMPEL REGEL: Gør kun teksten på download-knappen sort */
+    div[data-testid="stDownloadButton"] a {
         color: black !important;
-        background-color: #FF0000 !important;
-        text-decoration: none !important;
-        font-weight: 600 !important;
-        padding: 0.45em 0.8em !important;
-        display: inline-flex !important;
-        align-items: center !important;
-    }
-
-    div[data-testid="stDownloadButton"] > a:hover,
-    div[data-testid="stDownloadButton"] > a:active {
-        border-color: #A00 !important;
-        background-color: #D00000 !important;
-        color: black !important;
-        text-decoration: none !important;
     }
 
     </style>
